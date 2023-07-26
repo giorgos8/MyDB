236001eada7497e85765d65e0951477b8baad9cc
Georgios Kokkinos <georgios.kokkinos@ambience.gr>
2023-07-26 11:51:49 +0300

    alter-procedure-mothername

--- dbo/Stored Procedures/Procedure.sql
+++ dbo/Stored Procedures/Procedure.sql
@@ -2,5 +2,5 @@
 CREATE PROCEDURE [dbo].[Procedure]
 	
 AS
-	SELECT 'giorgos' as name, 'kokkinos' as surname, 'emmanouil' as fathername
+	SELECT 'giorgos' as name, 'kokkinos' as surname, 'emmanouil' as fathername, 'aikaterini' as mothername
 RETURN 0
